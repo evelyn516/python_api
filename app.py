c20fcf7f2d8c91e3ff00b57b19e84319d0aa72dc
Peter Ly <peter.ly1@hotmail.com>
2022-05-10 16:38:01 +0100

    made changes

--- app.py
+++ app.py
@@ -47,6 +47,35 @@ def frog():
     if request.method == 'POST':
         return f"{frog['name']} has joined the party" """
 
+@app.route('/frogs', methods=['GET', 'POST'])
+def frogs():
+    if request.method == 'GET':
+        return jsonify({"frogs" : froggies})
+    if request.method == 'POST':
+        flash('New froggo added!')
+        new_frog = {
+        "id": len(froggies),
+        "name": request.form["name"],
+        "type": request.form["type"]
+        }
+
+        froggies.append(new_frog)
+        
+        return jsonify({'frog': new_frog})
+    else:
+        return render_template('home.html', frogs=froggies)
+        
+
+# @app.route('/anime', methods=['GET', 'POST'])
+# def anime():
+#     if request.method == 'GET':
+#         return jsonify(animes)  
+#     if request.method == 'POST':
+#         data = request.json
+#         data['id'] = len(animes) + 1
+#         animes.append(data)
+#         return f"{data['name']} has been added to the anime list!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
